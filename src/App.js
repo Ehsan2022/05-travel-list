3dccb88e72e3afb6b27e5053826967bd9371fad9
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-06 22:06:32 +0430

    PackingList.js file created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Logo } from "./Logo";
 import { Form } from "./Form";
+import { PackingList } from "./PackingList";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -39,48 +40,7 @@ export default function App() {
   );
 }
 
-function PackingList({
-  items,
-  handleDeleteItem,
-  handleToggleItem,
-  handelClearList,
-}) {
-  const [sortBy, setSortBy] = useState("input");
-  let sortedItems;
-
-  if (sortBy === "input") sortedItems = items;
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-
-  return (
-    <div className="list">
-      <ul>
-        {sortedItems.map((item) => (
-          <Item
-            item={item}
-            key={item.id}
-            handleDeleteItem={handleDeleteItem}
-            handleToggleItem={handleToggleItem}></Item>
-        ))}
-      </ul>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={handelClearList}>Clear List</button>
-      </div>
-    </div>
-  );
-}
-function Item({ item, handleDeleteItem, handleToggleItem }) {
+export function Item({ item, handleDeleteItem, handleToggleItem }) {
   return (
     <li>
       <input
