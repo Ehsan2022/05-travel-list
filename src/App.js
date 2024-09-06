1cf7f2ab0044ca76e41941082678fab65a0b20b9
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-06 22:06:08 +0430

    Form.js file created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Logo } from "./Logo";
+import { Form } from "./Form";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -38,45 +39,6 @@ export default function App() {
   );
 }
 
-function Form({ handleAddItem }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!description) return;
-    const newItem = {
-      description: description,
-      quantity: quantity,
-      id: Date.now(),
-      packed: false,
-    };
-    handleAddItem(newItem);
-    setDescription("");
-    setQuantity(1);
-  }
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What dou you need for your ♥ trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="item..."
-      />
-      <button>Add</button>
-    </form>
-  );
-}
 function PackingList({
   items,
   handleDeleteItem,
