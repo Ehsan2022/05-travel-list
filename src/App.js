930c53a0379da526a028caa1dedebe7727ef4a73
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-09-06 22:10:00 +0430

    Stats.js file creted

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import  Logo  from "./Logo";
 import  Form  from "./Form";
 import  PackingList  from "./PackingList";
+import { Stats } from "./Stats";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -40,21 +41,4 @@ export default function App() {
   );
 }
 
-function Stats({ items }) {
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((numPacked / numItems) * 100);
-  return (
-    <footer className="stats">
-      <em>
-        {numItems === 0
-          ? "Start adding some items to your packing list 🚀"
-          : percentage === 100
-          ? "You got everything! Ready to go ✈️"
-          : `You have ${numItems} items on your list, and you already packed ${numPacked} (${
-              percentage === 0 ? "0" : percentage
-            }%)`}
-      </em>
-    </footer>
-  );
-}
+
